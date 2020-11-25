39d4ba0fb1c4663806ab0ba59d3e350b0b065d6c
Pedro Roquetta <PedroRoquetta>
2020-11-25 11:51:32 -0300

    funca algo?

--- Juego.php
+++ Juego.php
@@ -1,4 +1,12 @@
 <?php
 
 //a ver si funciona
-echo "hola";
+echo "Bienvenido al juego del ahorcado \n";
+echo "pulse:\n1 - Para jugar con una palabra aleatoria\n
+    2 - Para jugar con palabra a eleccion\n
+    3 - Agregar palabra al listado\n
+    4 - Mostrar informacion del juego\n
+    5 - Mostrar informacion completa del del primer juego con mas puntaje\n
+    6 - Mostrar la información completa del primer juego que supere un puntaje indicado por el usuario\n
+    7 - Mostrar la lista de palabras ordenada por orden alfabético\n
+    8 - Salir";
